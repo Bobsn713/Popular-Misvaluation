6a09b5d55098008fb2bbccd05f6ca132302403d8
Brant Wilson <brantwilson713@gmail.com>
2024-07-26 17:47:05 -0400

    Adding Comments

--- pop_misv.py
+++ pop_misv.py
@@ -4,6 +4,7 @@ import numpy as np
 #5 is arbitrary
 investor_guesses_sd = 5
 
+#8-22 establishes a dictionary of all the securities, their fake_values, and their prices
 #initializing
 securities = {}
 security_names = []
@@ -22,14 +23,14 @@ for name in security_names:
 
 
 
-#We have to do a for loop through the securities dictionary (this should be easier than for all the investors)
+#27-30 makes a dictionary with the perfect investor's valuation on all the securities (at some point it may make sense to add this to the investors dictionary)
 perfect_fv_investor = {}
 
 for security in security_names:
      perfect_fv_investor[f"{security}_estimate"]  = securities[security]["fake_value"] 
 
 
-
+#34-49 establishes a dictionary of investors, each of whom has estimates of the fake_value of each security
 #initializing
 investors = {}
 investor_names = []
@@ -41,7 +42,7 @@ for i in range(n_investors):
     investor_names.append(f"investor{i}")
 
 #use the list of n investor names as keys in the investors dictionary, with values that 
-#are randomly generated estimates of security 1's fake value
+#are randomly generated estimates of security n's fake value
 for name in investor_names:
     investors[name] = {}
     for security in security_names:
