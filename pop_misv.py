eb2483f4fa62d39a2cb5f1f1eda228a9b1f5b651
Brant Wilson <brantwilson713@gmail.com>
2024-07-26 17:03:34 -0400

    Made Securities Dict

--- pop_misv.py
+++ pop_misv.py
@@ -4,7 +4,25 @@ import numpy as np
 #5 is arbitrary
 investor_guesses_sd = 5
 
-security_1 = {
+#initializing
+securities = {}
+security_names = []
+
+n_securities = 3 #I can choose this
+
+#make a list of n securities
+for i in range(n_securities):
+    security_names.append(f"security{i}")
+
+#use the list of n investor names as keys in the investors dictionary, with values that 
+#are randomly generated estimates of security 1's fake value
+for name in security_names:
+    securities[name] = {"fake_value": random.randrange(0,100),"price": random.randrange(0,100)}
+
+
+print(securities)
+
+"""security_1 = {
     "fake_value" : random.randrange(0,100), #cap at 100 is arbitrary (should this be a normal distribution?),
     "price" : random.randrange(0,100), #cap at 100 is arbitrary (should this be a normal distribution?)
 }
@@ -27,7 +45,9 @@ for i in range(n_investors):
 #use the list of n investor names as keys in the investors dictionary, with values that 
 #are randomly generated estimates of security 1's fake value
 for name in investor_names:
-    investors[name] = {"security_1_estimate": np.random.normal(security_1["fake_value"],investor_guesses_sd)} #the[0] is because it returns as an array and I don't want an array
+    investors[name] = {"security_1_estimate": np.random.normal(security_1["fake_value"],investor_guesses_sd)}
+"""
+    
 
 """ If you want to test
 
